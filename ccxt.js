00e14886bc985eab907f5a0b95ba8610853d701b
yufengwei <150238471@qq.com>
2018-11-23 14:24:37 +0800

    Update ccxt.js

--- ccxt.js
+++ ccxt.js
@@ -75,6 +75,7 @@ const exchanges = {
     'bitz':                    require ('./js/bitz.js'),
     'bl3p':                    require ('./js/bl3p.js'),
     'bleutrade':               require ('./js/bleutrade.js'),
+    'boombit':                 require ('./js/boombit.js'),
     'braziliex':               require ('./js/braziliex.js'),
     'btcalpha':                require ('./js/btcalpha.js'),
     'btcbox':                  require ('./js/btcbox.js'),
