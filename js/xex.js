e853285be9ab791b8fd131775d16f3eb687105eb
yufengwei <150238471@qq.com>
2018-12-06 14:33:31 +0800

    Update xex.js

--- js/xex.js
+++ js/xex.js
@@ -18,13 +18,13 @@ module.exports = class xex extends Exchange {
                 'fetchTicker': true,
                 'fetchOHLCV': true,
                 'fetchOrderBook': true,
-				'fetchTrades': true,
+                'fetchTrades': true,
                 'fetchMyTrades': true,
                 'fetchOrderBooks': true,
-				'fetchOpenOrders': true,
+                'fetchOpenOrders': true,
                 'fetchOrder': true,
                 'createOrder': true,
-				'cancelOrder': true,
+                'cancelOrder': true,
                 'fetchBalance': true,
             },
             'timeframes': {
@@ -33,35 +33,35 @@ module.exports = class xex extends Exchange {
                 '15m': '15MIN',
                 '30m': '30MIN',
                 '1h': '1H',
-				'2h': '2H',
+                '2h': '2H',
                 '4h': '4H',
                 '6h': '6H',
                 '12h': '12H',
                 '1d': 'D',
-				'2d': '2D',
-				'1w': 'W',
-				'month': 'MONTH',
+                '2d': '2D',
+                '1w': 'W',
+                'month': 'MONTH',
             },
             'urls': {
-                'logo': '',
-                'api': 'https://api.xex-dev.com',
-                'www': 'http://www.xex-dev.com/cross/home',
-                'doc': 'https://xex.zendesk.com/hc/en-us/categories/360001030591',
+                'logo': 'https://www.crossexchange.io/images/logo_icon.png',
+                'api': 'https://api.crossexchange.io',
+                'www': 'https://www.crossexchange.io/cross/home',
+                'doc': 'https://support.crossexchange.io/hc/en-us/categories/360001030591?flash_digest=4496738595f09128fc199486ac8f0fcee028b0ab',
             },
             'api': {
                 'public': {
                     'get': [
                         'GET/v1/api/ticker',
-						'GET/v1/api/kline',
-						'GET/v1/api/depth',
-						'GET/v1/api/trades',
+                        'GET/v1/api/kline',
+                        'GET/v1/api/depth',
+                        'GET/v1/api/trades',
                     ],
                 },
                 'private': {
                     'get': [
-						'GET/v1/api/orderdetail',
-						'GET/v1/api/auth/wallet',
-						'GET/v1/api/mineLimit',
+                        'GET/v1/api/orderdetail',
+                        'GET/v1/api/auth/wallet',
+                        'GET/v1/api/mineLimit',
                     ],
                        
                     'post': [
@@ -83,7 +83,7 @@ module.exports = class xex extends Exchange {
             { 'id': 'LTC_USDT', 'symbol': 'LTC_USDT', 'base': 'USDT', 'quote': 'LTC' },
             { 'id': 'BCH_USDT', 'symbol': 'BCH_USDT', 'base': 'USDT', 'quote': 'BCH' },
             { 'id': 'XRP_USDT', 'symbol': 'XRP_USDT', 'base': 'USDT', 'quote': 'XRP' },
-			{ 'id': 'XEX_USDT', 'symbol': 'XEX_USDT', 'base': 'USDT', 'quote': 'XEX' },
+            { 'id': 'XEX_USDT', 'symbol': 'XEX_USDT', 'base': 'USDT', 'quote': 'XEX' },
             { 'id': 'ETH_BTC', 'symbol': 'ETH_BTC', 'base': 'BTC', 'quote': 'ETH' },
             { 'id': 'LTC_BTC', 'symbol': 'LTC_BTC', 'base': 'BTC', 'quote': 'LTC' },
             { 'id': 'XRP_BTC', 'symbol': 'XRP_BTC', 'base': 'BTC', 'quote': 'XRP' },
@@ -96,48 +96,28 @@ module.exports = class xex extends Exchange {
     async fetchBalance (params = {}) {
         await this.loadMarkets ();
         let response = await this.privateGetGETV1ApiAuthWallet (params);
-        //	{
-		//		"code": 0,
-		//		"data": 
-		//		{
-		//			"free": 
-		//			{
- 		//				"BTC": "9.99094840",
- 		//				"ETH": "10.21587000",
- 		//				"USDT": "199.80000000"
- 	    //		   	},
-		//			"freezed": 
-		//			{
- 		//				"BTC": "0.16420000",
- 		//				"ETH": "0.00000000",
- 		//				"USDT": "0.00000000"
- 		//		     }
-		//		},
-		//		"channel": "auth-wallet"
-		//	}
-		
         let result = { 'info': response };
-		let free = response['data']["free"];
-		let freezed = response['data']["freezed"];
-		let coinTypes = Object.keys (free);
+        let free = response['data']["free"];
+        let freezed = response['data']["freezed"];
+        let coinTypes = Object.keys (free);
         for (let i = 0; i < coinTypes.length; i++) 
-		{
-			let coin_type = coinTypes[i].toUpperCase ();
+        {
+            let coin_type = coinTypes[i].toUpperCase ();
             let account = this.account ();
             account['free'] = parseFloat (free[coin_type]);
-			account['used'] = parseFloat (freezed[coin_type]);
-			account['total'] = this.sum (account['free'], account['used']);
-			result[coin_type] = account;	
-		}
-				
+            account['used'] = parseFloat (freezed[coin_type]);
+            account['total'] = this.sum (account['free'], account['used']);
+            result[coin_type] = account;       
+        }
+
         return this.parseBalance (result);
     }
 
     async fetchOrderBook (symbol, limit = undefined, params = {}) {
-		if (symbol === undefined)
+        if (symbol === undefined)
             throw new ExchangeError (this.id + ' fetchOrderBook() requires a symbol argument');
-		
-		await this.loadMarkets ();
+
+        await this.loadMarkets ();
         let response = await this.publicGetGETV1ApiDepth (this.extend ({
              'pair': this.marketId (symbol),
         }, params));
@@ -148,11 +128,11 @@ module.exports = class xex extends Exchange {
         if (symbol === undefined)
             throw new ExchangeError (this.id + ' fetchTicker() requires a symbol argument');
         
-		await this.loadMarkets ();
+await this.loadMarkets ();
         let response = await this.publicGetGETV1ApiTicker (this.extend ({
             'pair': this.marketId (symbol),
         }, params));
-		
+        
         let ticker = response['data'];
         let last = this.safeFloat (ticker, 'last');
         return {
@@ -183,7 +163,7 @@ module.exports = class xex extends Exchange {
         if (symbol === undefined)
             throw new ExchangeError (this.id + ' fetchOHLCV() requires a symbol argument');
 
-		await this.loadMarkets ();
+        await this.loadMarkets ();
         let market = this.market (symbol);
         let response = await this.publicGetGETV1ApiKline (this.extend ({
             'pair': market['id'],
@@ -192,16 +172,16 @@ module.exports = class xex extends Exchange {
         let ohlcvs = response['data'];
         return this.parseOHLCVs (ohlcvs, market, timeframe, since, limit);
     }
-	
-	parseTrade (trade, market) {
-		let side_flag = "sell"
-		let price = this.asFloat(trade[1]);
-		if(price > 0)
+       
+       parseTrade (trade, market) {
+        let side_flag = "sell"
+        let price = this.asFloat(trade[1]);
+        if(price > 0)
             side_flag = 'buy';
         let timestamp = trade[3];
-		let fee_num = undefined;
-		if(trade.length >= 7)
-			fee_num = this.asFloat(trade[6]);
+        let fee_num = undefined;
+        if(trade.length >= 7)
+               fee_num = this.asFloat(trade[6]);
         return {
             'id': trade[0],
             'info': trade,
@@ -213,7 +193,7 @@ module.exports = class xex extends Exchange {
             'side': side_flag,
             'price': price,
             'amount': this.asFloat(trade[2]),
-			'fee': fee_num,
+               'fee': fee_num,
         };
     }
 
@@ -221,51 +201,51 @@ module.exports = class xex extends Exchange {
         if (symbol === undefined)
             throw new ExchangeError (this.id + ' fetchTrades() requires a symbol argument');
 
-		await this.loadMarkets ();
+        await this.loadMarkets ();
         let market = this.market (symbol);
         let response = await this.publicGetGETV1ApiTrades (this.extend ({
             'pair': market['id'],
         }, params));
         let trades = response['data']
-		let newTrades = new Array();
-		for(let i = 0; i < trades.length; i++)
-		{
-			let oneTrade = new Array();
-			for(let k = 0; k < trades[i].length; k++)
-			{
-				oneTrade.push(trades[i][k]);
-			}
-			oneTrade.push(symbol);
-			newTrades.push(oneTrade)
-		}
+        let newTrades = new Array();
+        for(let i = 0; i < trades.length; i++)
+        {
+               let oneTrade = new Array();
+               for(let k = 0; k < trades[i].length; k++)
+               {
+                oneTrade.push(trades[i][k]);
+               }
+               oneTrade.push(symbol);
+               newTrades.push(oneTrade)
+        }
         return this.parseTrades (newTrades, market, since, limit);
     }
-	
-	async fetchMyTrades (symbol = undefined, since = undefined, limit = undefined, params = {}) {
+       
+       async fetchMyTrades (symbol = undefined, since = undefined, limit = undefined, params = {}) {
         if (symbol === undefined)
             throw new ExchangeError (this.id + ' fetchMyTrades() requires a symbol argument');
 
         await this.loadMarkets ();
         let response = await this.privatePostPOSTV1ApiTrades (params);
-		let trades = response['data']
-		let newTrades = new Array();
-		for(let i = 0; i < trades.length; i++)
-		{
-			let oneTrade = new Array();
-			oneTrade.push(trades[i][0]);	//	0: trade id
-			oneTrade.push(trades[i][3]);	// 	1: price
-			oneTrade.push(trades[i][4]);	//	2: amount
-			oneTrade.push(trades[i][2]);	//	3: time
-			oneTrade.push(trades[i][6]);	// 	4: type
-			oneTrade.push(trades[i][1]);	//	5: pair
-			oneTrade.push(trades[i][8]);	//	6: fee
-			newTrades.push(oneTrade)
-		}
-		
+        let trades = response['data']
+        let newTrades = new Array();
+        for(let i = 0; i < trades.length; i++)
+        {
+               let oneTrade = new Array();
+               oneTrade.push(trades[i][0]);       //       0: trade id
+               oneTrade.push(trades[i][3]);       //        1: price
+               oneTrade.push(trades[i][4]);       //       2: amount
+               oneTrade.push(trades[i][2]);       //       3: time
+               oneTrade.push(trades[i][6]);       //        4: type
+               oneTrade.push(trades[i][1]);       //       5: pair
+               oneTrade.push(trades[i][8]);       //       6: fee
+               newTrades.push(oneTrade)
+        }
+        
         return this.parseTrades (newTrades, undefined, since, limit);
     }
-	
-	parseOrderStatus (status) {
+       
+       parseOrderStatus (status) {
         let statuses = {
             '1': 'Start',
             '2': 'Partially Executed', // partially filled
@@ -278,7 +258,7 @@ module.exports = class xex extends Exchange {
         return status;
     }
 
-	async fetchOrder (id, symbol = undefined, params = {}) {
+       async fetchOrder (id, symbol = undefined, params = {}) {
         if (id === undefined)
             throw new ExchangeError (this.id + ' fetchOrder() requires a id argument');
 
@@ -290,12 +270,12 @@ module.exports = class xex extends Exchange {
         let order = response['data'];
         let timestamp = order[2];
         let status = this.parseOrderStatus (order[9]);
-		let total = this.asFloat (order[4]);
-		let comleted = this.asFloat (order[3]);
-		let remain = total - comleted;
-		let sidex = 'sell'
-		let price = this.asFloat(order[5]);
-		if(price > 0)
+        let total = this.asFloat (order[4]);
+        let comleted = this.asFloat (order[3]);
+        let remain = total - comleted;
+        let sidex = 'sell'
+        let price = this.asFloat(order[5]);
+        if(price > 0)
             sidex = 'buy';
         let result = {
             'info': order,
@@ -314,38 +294,38 @@ module.exports = class xex extends Exchange {
             'remaining': remain,
             'status': status,
             'fee': undefined,
-			'stop_price': order[8],
+               'stop_price': order[8],
         };
         return result;
     }
-	
-	async fetchOrderBooks (symbols = undefined, params = {}) {
-		await this.loadMarkets ();
+       
+       async fetchOrderBooks (symbols = undefined, params = {}) {
+        await this.loadMarkets ();
         let response = await this.privatePostPOSTV1ApiOrders (params);
         if ('data' in response) 
-		{
+        {
             return this.parseOrders (response['data'], undefined, undefined, undefined);
         }
         return [];
     }
-	
-	parseOrder (order, market = undefined) {
+       
+       parseOrder (order, market = undefined) {
         let id = order[0];
         let timestamp = order[2];
         let symbol = order[1];
-		let filled = this.asFloat(order[3]);
+        let filled = this.asFloat(order[3]);
         let amount = this.asFloat(order[4]);
-		let side = 'sell'
-		let price = this.asFloat(order[5]);
-		if(price > 0)
+        let side = 'sell'
+        let price = this.asFloat(order[5]);
+        if(price > 0)
             side = 'buy';
         let remaining = amount - filled;
-		let tradePrice = this.asFloat(order[6]);
-		let cost = filled * tradePrice;
+        let tradePrice = this.asFloat(order[6]);
+        let cost = filled * tradePrice;
         let type = order[7];
-		let stopPrice = this.asFloat(order[8])
+        let stopPrice = this.asFloat(order[8])
         let status = this.parseOrderStatus (order[9]);
-		
+        
         let result = {
             'info': order,
             'id': id,
@@ -363,23 +343,23 @@ module.exports = class xex extends Exchange {
             'remaining': remaining,
             'status': status,
             'fee': undefined,
-			'stop_price':stopPrice,
+               'stop_price':stopPrice,
         };
         return result;
     }
-	
-	async fetchOpenOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
+       
+       async fetchOpenOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
         await this.loadMarkets ();
         let response = await this.privatePostPOSTV1ApiAuthOrders ();
-		if ('data' in response) 
-		{
+        if ('data' in response) 
+        {
             return this.parseOrders (response['data'], undefined, since, limit);
         }
         return [];
     }
-	
+       
     async createOrder (symbol, type, side, amount, price = undefined, params = {}) {
-		await this.loadMarkets ();
+        await this.loadMarkets ();
         let order = {
             'isbid': side,
             'order_type': type,
@@ -388,8 +368,8 @@ module.exports = class xex extends Exchange {
         };
         if (type === 'STOP-LIMIT')
             order['stop_price'] = price;
-		else
-			order['stop_price'] = 0;
+        else
+               order['stop_price'] = 0;
         let result = await this.privatePostPOSTV1ApiPlaceOrder (this.extend (order, params));
         return {
             'info': result,
@@ -398,17 +378,17 @@ module.exports = class xex extends Exchange {
     }
 
     async cancelOrder (id, symbol = undefined, params = {}) {
-		if (symbol === undefined)
+        if (symbol === undefined)
             throw new ExchangeError (this.id + ' cancelOrder() requires a symbol argument');
-		
-		await this.loadMarkets ();
+        
+        await this.loadMarkets ();
         return await this.privatePostPOSTV1ApiCancelOrder(this.extend ({
             'order_id': id,
-			'pair': this.marketId (symbol),
+               'pair': this.marketId (symbol),
         }, params));
     }
-	
-	nonce () {
+       
+       nonce () {
         return this.milliseconds ();
     }
 
@@ -421,25 +401,25 @@ module.exports = class xex extends Exchange {
                 url += '?' + this.urlencode (params);
         } else {
             this.checkRequiredCredentials ();
-			let time = this.nonce();
-			let query = this.keysort (this.extend ({
+               let time = this.nonce();
+               let query = this.keysort (this.extend ({
                 'api_key': this.apiKey,
                 'auth_nonce': time,
             }, params));
-					
-			let str = ''
-			Object.keys(query).forEach(function(key)
-			{
-				str += query[key]
-			});
-			
-			let signed = this.hash (this.encode(str + this.secret));
-			if(method === 'GET')
-			{
-				signed = this.hash (this.encode(this.apiKey + time + this.secret));
-			}
-			
-			let query_str = this.urlencode(query);
+                       
+               let str = ''
+               Object.keys(query).forEach(function(key)
+               {
+                str += query[key]
+               });
+               
+               let signed = this.hash (this.encode(str + this.secret));
+               if(method === 'GET')
+               {
+                signed = this.hash (this.encode(this.apiKey + time + this.secret));
+               }
+               
+               let query_str = this.urlencode(query);
             url += '?' + query_str + '&auth_sign=' + signed;
             headers = { 'Content-Type': 'application/json' };
         }
